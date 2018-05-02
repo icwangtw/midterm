685080da4efe612cd497082c53b6007b0aa16b10
bill <billwu_@outlook.com>
2018-05-02 21:38:50 +0000

    finished adding Fkey to all tables

--- db/migrations/20180502205820_food_orders.js
+++ db/migrations/20180502205820_food_orders.js
@@ -4,12 +4,14 @@ exports.up = function(knex, Promise) {
       .createTable('food_orders', function(table){
         table.increments('id').primary();
         table.integer('quantity');
+        table.integer('food_id');
+        table.integer('order_id');
       })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('food_orders').dropTable('famous_people')
+    knex.schema.dropTable('food_orders')
   ]);
 };
