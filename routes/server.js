8b5a7d9c67e68eeee2975820f05fa365d1fff2c6
Stephen <stephenicwang@gmail.com>
2018-05-03 03:40:43 +0000

    server update

--- routes/server.js
+++ routes/server.js
@@ -4,16 +4,26 @@ const port = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcrypt");
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 //middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieSession({
   name: "session",
   keys: ["2o9UjHxdpd7PKrPFj0B1", "y28EbKtnaSaYpDGdTsth", "7igwx7myeIB9vbYncwJo"]
 }));
 app.set("view engine", "ejs");
 
+//render menu
 app.get("/", (req, res) => {
-    let templateVars = {urls: filteredDatabase, theUser: usersDatabase[req.session.id]};
-    res.render("urls_index", templateVars);
+    let templateVars = {};
+    res.render("index", templateVars);
+});
+
+//sms inbound
+
+//sms outbound
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
